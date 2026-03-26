48ef3b23a3579297ce6d7df6945df0bc73b02478
abhay <avittal31@gatech.edu>
2026-03-26 16:49:34 -0400

    working gpio test attempt 6-3

--- GpioInputs.cpp
+++ GpioInputs.cpp
@@ -14,8 +14,8 @@ GpioInputs::GpioInputs() :
         .add_line_settings(PINA, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT).set_active_low(true))
         .add_line_settings(PINB, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT).set_active_low(true))
         .add_line_settings(PINX, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT).set_active_low(true))
-        .add_line_settings(PINDIALCLK, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT).set_active_low(true).set_edge_detection(::gpiod::line_settings::edge_detection::BOTH))
-        .add_line_settings(PINDIALDT, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT).set_active_low(true).set_edge_detection(::gpiod::line_settings::edge_detection::BOTH))
+        .add_line_settings(PINDIALCLK, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT).set_active_low(true).set_edge_detection(::gpiod::line_settings::edge::BOTH))
+        .add_line_settings(PINDIALDT, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT).set_active_low(true).set_edge_detection(::gpiod::line_settings::edge::BOTH))
         .do_request()),
     dialThread([&] () {
         // Transition table: [prevState][currState] -> Dial result
@@ -28,7 +28,7 @@ GpioInputs::GpioInputs() :
             {Dial::NEUTRAL, Dial::DOWN, Dial::UP, Dial::NEUTRAL}   // curr=11
         };
         while (dialRunning) {
-            gpioLines.event_wait(std::chrono::milliseconds(100));
+            gpioLines.wait_edge_events(std::chrono::milliseconds(100));
             ::gpiod::edge_event_buffer events(16);
             gpioLines.read_edge_events(events);
             for (const auto& event : events) {
