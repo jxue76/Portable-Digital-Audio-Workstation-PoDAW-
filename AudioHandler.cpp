31b6e44d6eea1b51547aec2a38af9c43178eb6f3
abhay <avittal31@gatech.edu>
2026-04-19 16:58:33 -0400

    device id fix attempt 2

--- AudioHandler.cpp
+++ AudioHandler.cpp
@@ -23,7 +23,6 @@ int audio_callback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFr
 }
 
 AudioHandler::AudioHandler() {
-    std::cout << audio.getDeviceIds() << std::endl;
     outputParams.deviceId = 0;
     outputParams.nChannels = 1; // Mono output
     audio.openStream(&outputParams, nullptr, RTAUDIO_FLOAT32, 48000, &bufferFrames, &audio_callback, this);
