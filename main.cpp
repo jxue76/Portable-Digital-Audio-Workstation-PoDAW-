dbbb4cf51b554f6fd802a237bf7cda000376b174
JasonJiyoongLee <jlee3984@gatech.edu>
2026-04-24 17:29:01 -0400

    glfw 3.2

--- main.cpp
+++ main.cpp
@@ -116,7 +116,7 @@ int main(int, char**) {
 
     //GLFWwindow* window = glfwCreateWindow(480, 320, "PoDAW Interface", nullptr, nullptr);
     GLFWwindow* window = glfwCreateWindow(480, 320, "PoDAW Interface", glfwGetPrimaryMonitor(), nullptr);
-    //if (!window) return 1;
+    if (!window) return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
