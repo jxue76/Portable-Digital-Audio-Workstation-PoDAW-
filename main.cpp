8f8bab11df6f91b7196d4dffff7c42bd03a81b0f
JasonJiyoongLee <jlee3984@gatech.edu>
2026-04-25 16:24:36 -0400

    fix.

--- main.cpp
+++ main.cpp
@@ -228,11 +228,11 @@ int main(int, char**) {
             }
             input_delay = std::chrono::high_resolution_clock::now();
             input_lock = true;
-        } else if (inputs.getDialPosition() == Dial::DOWN && !input_loc) {
+        } else if (inputs.getDialPosition() == Dial::DOWN && !input_lock) {
             if (piano->getVolume() > 0.0f) {
                 piano->setVolume(piano->getVolume() - 0.02f);
-                guitar->setVolume(guitar->setVolume() - 0.02f);
-                drums->setVolume(drums->setVolume() - 0.02f);
+                guitar->setVolume(guitar->getVolume() - 0.02f);
+                drums->setVolume(drums->getVolume() - 0.02f);
                 bass->setVolume(bass->getVolume() - 0.02f);
             }
             input_delay = std::chrono::high_resolution_clock::now();
